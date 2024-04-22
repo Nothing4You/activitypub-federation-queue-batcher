31430a3b257111f36cc82a7d6929d02f958fc0db
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2024-04-22 02:06:27 +0200

    batch-receiver: Fix status code check for submitting activities to destination

--- src/activitypub_federation_queue_batcher/_apub_helpers.py
+++ src/activitypub_federation_queue_batcher/_apub_helpers.py
@@ -1,2 +1,2 @@
 def is_tolerable_activity_submission_status_code(status: int, /) -> bool:
-    return status in (408, 429) or status >= 500  # noqa: PLR2004
+    return status not in (408, 429) and status < 500  # noqa: PLR2004
